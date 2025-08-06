76bf5f8400c08b75b5be1c6d44b91efd56310330
izlovchiismoil <iizlovchi@gmail.com>
2025-08-06 14:35:52 +0500

    login signup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LoginSignup from "./Components/LoginSignup/LoginSignup.jsx";
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <LoginSignup />
+      </div>
+    </>
+  )
+}
+
+export default App
